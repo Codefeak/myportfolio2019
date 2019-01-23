bef830e08ba873577c45d3519796e91a1a58bbe1
codefeak <codefever17@gmail.com>
2019-01-23 22:46:03 +0200

    Updates

---
+++ path---portfolio-kirk-ee27293e846b997c6466.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x76a2e1cf07b4],{436:function(t,e){t.exports={data:{markdownRemark:{html:"<p>KIRK was created as a star trek based chatbot developed for command line access with C#.\nThe resulting chatbot can be trained through conversation or via parsing of text files with full text-to-speech output.</p>",timeToRead:1,frontmatter:{title:"KIRK",date:"2018-01-08",tags:["C#","bot"]}}},pathContext:{slug:"/portfolio/kirk/"}}}});
+//# sourceMappingURL=path---portfolio-kirk-ee27293e846b997c6466.js.map
